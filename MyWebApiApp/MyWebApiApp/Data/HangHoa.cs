73e52b9fd2c7e8407b1a04619af42ca3ff9bf9fe
Tuong Huynh <41580247+huynhdieutuong@users.noreply.github.com>
2023-01-20 16:35:30 +0700

    Fluent API

--- MyWebApiApp/MyWebApiApp/Data/HangHoa.cs
+++ MyWebApiApp/MyWebApiApp/Data/HangHoa.cs
@@ -25,5 +25,11 @@ namespace MyWebApiApp.Data
         public int? MaLoai { get; set; }
         [ForeignKey("MaLoai")]
         public Loai Loai { get; set; }
+
+        public ICollection<DonHangChiTiet> DonHangChiTiets { get; set; }
+        public HangHoa()
+        {
+            DonHangChiTiets = new List<DonHangChiTiet>();
+        }
     }
 }
