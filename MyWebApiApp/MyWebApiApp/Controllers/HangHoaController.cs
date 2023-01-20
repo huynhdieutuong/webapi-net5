8d767559110d5655bc328f2869c89d62b93a71bf
Tuong Huynh <41580247+huynhdieutuong@users.noreply.github.com>
2023-01-20 11:13:51 +0700

    CRUD Product

---
+++ MyWebApiApp/MyWebApiApp/Controllers/HangHoaController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hangHoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+            hangHoas.Add(hangHoa);
+            return Ok(new
+            {
+                Success = true,
+                Data = hangHoa
+            });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                var hangHoa = hangHoas.FirstOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null) return NotFound();
+                return Ok(hangHoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditById(string id, HangHoaVM hangHoaVM)
+        {
+            if (id == null) return BadRequest();
+            try
+            {
+                var hangHoa = hangHoas.FirstOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null) return NotFound();
+                hangHoa.TenHangHoa = hangHoaVM.TenHangHoa;
+                hangHoa.DonGia = hangHoaVM.DonGia;
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteById(string id)
+        {
+            if (id == null) return BadRequest();
+            try
+            {
+                var hangHoa = hangHoas.FirstOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null) return NotFound();
+                hangHoas.Remove(hangHoa);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
